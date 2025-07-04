08442c0a64e8394ae66925aca8a1abb4f9790569
Genadi V. Zawidowski <mgs2001@mail.ru>
2025-07-04 08:09:18 +0300

    save state

--- src/portable/chipidea/ci_hs/hcd_ci_hs.c
+++ src/portable/chipidea/ci_hs/hcd_ci_hs.c
@@ -62,7 +62,7 @@ bool hcd_dcache_clean_invalidate(void const* addr, uint32_t data_size) {
 #include "ci_hs_lpc18_43.h"
 
 #else
-#error "Unsupported MCUs"
+//#error "Unsupported MCUs"
 #endif
 
 //--------------------------------------------------------------------+
@@ -81,19 +81,35 @@ bool hcd_init(uint8_t rhport, const tusb_rhport_init_t* rh_init) {
   hcd_reg->USBCMD |= USBCMD_RESET;
   while ( hcd_reg->USBCMD & USBCMD_RESET ) {}
 
+#if CFG_TUSB_MCU == OPT_MCU_F1C100S
+
+
+  // FIXME force full speed, still have issue with Highspeed enumeration
+  // probably due to physical connection bouncing when plug/unplug
+  // 1. Have issue when plug/unplug devices, maybe the port is not reset properly
+  // 2. Also does not seems to detect disconnection
+  //hcd_reg->PORTSC1 |= PORTSC1_FORCE_FULL_SPEED;
+
+    const uintptr_t opregspacebase = (uintptr_t) CI_HS_REG(rhport)
+            + (* (volatile uint32_t *) CI_HS_REG(rhport) & 0x00FF);
+
+      //return ehci_init(rhport, (uint32_t) &hcd_reg->CAPLENGTH, (uint32_t) &hcd_reg->USBCMD);
+    return ehci_init(rhport, (uintptr_t) CI_HS_REG(rhport), opregspacebase);
+
   // Set mode to device, must be set immediately after reset
-#if CFG_TUSB_MCU == OPT_MCU_LPC18XX || CFG_TUSB_MCU == OPT_MCU_LPC43XX
+#elif CFG_TUSB_MCU == OPT_MCU_LPC18XX || CFG_TUSB_MCU == OPT_MCU_LPC43XX
   // LPC18XX/43XX need to set VBUS Power Select to HIGH
   // RHPORT1 is fullspeed only (need external PHY for Highspeed)
   hcd_reg->USBMODE = USBMODE_CM_HOST | USBMODE_VBUS_POWER_SELECT;
   if (rhport == 1) {
     hcd_reg->PORTSC1 |= PORTSC1_FORCE_FULL_SPEED;
   }
+  return ehci_init(rhport, (uint32_t) &hcd_reg->CAPLENGTH, (uint32_t) &hcd_reg->USBCMD);
 #else
   hcd_reg->USBMODE = USBMODE_CM_HOST;
+  return ehci_init(rhport, (uint32_t) &hcd_reg->CAPLENGTH, (uint32_t) &hcd_reg->USBCMD);
 #endif
 
-  return ehci_init(rhport, (uint32_t) &hcd_reg->CAPLENGTH, (uint32_t) &hcd_reg->USBCMD);
 }
 
 void hcd_int_enable(uint8_t rhport) {
